394e51c920f11894d62c2a22518491a00918d242
Vladislav Knyshov <inova136@gmail.com>
2022-08-15 20:16:16 +0300

    feat: update setup script

--- setup.bash
+++ setup.bash
@@ -1,33 +1,42 @@
 #!/bin/bash
 
 function log {
-  local RED='\033[1;36m'
+  local BLUE='\033[1;36m'
   local NC='\033[0m'
-  echo -e "\n${RED}[SETUP SCRIPT]: $1${NC}"
+  echo -e "${BLUE}[SETUP STEP $1]: $2${NC}"
 }
 
-log "Setup & link ping-server package"
+log 1 "Setup & link ping-server package"
 cd ./packages/ping-server
 npm i
 npm link
+log 1 "Done"
 
-log "Setup & link hermione-auto-start-selenium package"
+log 2 "Setup & link hermione-auto-start-selenium package"
 cd ../hermione-auto-start-selenium
 npm link ping-server
 npm i
 npm link
+log 2 "Done"
 
-log "Setup & link hermione-auto-start-project package"
+log 3 "Setup & link hermione-auto-start-project package"
 cd ../hermione-auto-start-project
 npm link ping-server
 npm i
 npm link
+log 3 "Done"
 
-log "Setup the whole project"
+log 4 "Setup the whole project"
 cd ../../
 npm i
 npm link hermione-auto-start-selenium hermione-auto-start-project ping-server
 nvm use
 
-log "Make sure you use node version specified in .nvmrc"
-log "With love, Vladislav"
+NODE_VERSION = $(cat ./.nvmrc)
+if [ $? -ne 0 ]; then
+ log 4 "Error while setting node version. Please, set node version $NODE_VERSION manually"
+ else 
+ log 4 "Successful set node version $NODE_VERSION"
+fi
+
+log 5 "With love, Vladislav"
